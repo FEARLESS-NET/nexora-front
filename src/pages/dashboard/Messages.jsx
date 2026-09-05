e3b42c9e518b171a4268af817aedc7236067e980
mirzoxid <mirzohidbahtiyorov5@gmail.com>
2026-09-06 02:15:27 +0500

    oijqdpoijqpo

--- src/pages/dashboard/Messages.jsx
+++ src/pages/dashboard/Messages.jsx
@@ -2384,7 +2384,8 @@ const Messages = () => {
 
           {/* MESSAGES */}
 
-          <div className="min-h-0 flex-1 space-y-3 overflow-y-auto p-4">
+          <div className="min-h-0 flex-1 overflow-y-auto p-4">
+            <div className="flex flex-col gap-3">
 
             <div className="flex justify-center">
               <span className="rounded-full bg-white/5 px-2 py-1 text-[10px] text-gray-600">
@@ -2576,6 +2577,7 @@ const Messages = () => {
               }
             />
 
+            </div>
           </div>
 
           {/* IMAGE PREVIEW */}
