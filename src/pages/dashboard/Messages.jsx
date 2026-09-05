803d3899057d88c728b0dc3bbb8b819c69c00e5f
mirzoxid <mirzohidbahtiyorov5@gmail.com>
2026-09-06 02:42:29 +0500

    pifwpqwdkqwje

--- src/pages/dashboard/Messages.jsx
+++ src/pages/dashboard/Messages.jsx
@@ -1041,7 +1041,7 @@ const Messages = () => {
     const interval =
       setInterval(() => {
         refreshCurrentConversation();
-      }, 5000);
+      }, 2000);
 
     return () => {
       clearInterval(interval);
